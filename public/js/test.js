e561339e9fe0ebbcd895410a2911903320e7bb04
nidhogg5 <nidhogg5@yahoo.com.tw>
2017-04-26 16:08:32 +0800

    測試頁面

--- public/js/test.js
+++ public/js/test.js
@@ -9,3 +9,4 @@ $( "#up" ).click(function() {
     $( "#year" ).html(i+'歲')
     $( "#date" ).html("今天日期是 " + myDate.getFullYear()+ " 年 " + (myDate.getMonth()+1) + " 月 " + myDate.getDate() + " 日")
 });
+
