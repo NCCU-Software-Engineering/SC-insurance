2d04826a3370473318e2c6a631df7d95da58298c
autumn <dennis456852@gmail.com>
2017-06-29 18:25:45 +0800

    edit ui+search method

--- public/index.js
+++ public/index.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
     $(".loader").hide();
-    $('.trans').prop('disabled', true);
     $.post("/getaccount", function(data) {
         data.forEach(function(element) {
             $('.select-account').append($('<option>', {
@@ -10,111 +9,27 @@ $(document).ready(function() {
             }));
         }, this);
     });
-    $("#accounts1").change(() => {
-        $('.trans').prop('disabled', true);
-    });
-    $("#compile").click(function() {
-        $(".loader").show();
-        $('button').prop('disabled', true);
-        $.post("/compile", function(data) {
-            $('button').prop('disabled', false);
-            $('.trans').prop('disabled', true);
-            $(".loader").hide();
-        });
-    });
-
-    $("#deploy").click(function() {
-        if ($("#accounts1 option:selected").text() == "") {
-            alert("Select one account !");
-        }
-        else {
-            $(".loader").show();
-            $('button').prop('disabled', true);
-            $.post("/deploy", {
-                account: $("#accounts1 option:selected").text()
-            }, function(data) {
-                $(".loader").hide();
-                $('button').prop('disabled', false);
-            });
-        }
-    });
-
-    $("#trans1").click(function() {
-        if ($("#accounts1 option:selected").text() == "") {
-            alert("Select one account !");
-        }
-        else {
-            $(".loader").show();
-            $('button').prop('disabled', true);
-            $.post("/trans", {
-                account: $("#accounts1 option:selected").text(),
-                time: 1
-            }, function(data) {
-                $(".loader").hide();
-                $('button').prop('disabled', false);
-            });
-        }
-    });
-
-    $("#trans2").click(function() {
-        if ($("#accounts1 option:selected").text() == "") {
-            alert("Select one account !");
-        }
-        else {
-            $(".loader").show();
-            $('button').prop('disabled', true);
-            $.post("/trans", {
-                account: $("#accounts1 option:selected").text(),
-                time: 2
-            }, function(data) {
-                $(".loader").hide();
-                $('button').prop('disabled', false);
-            });
-        }
-    });
-
-    $("#accounts2").change(() => {
-        $('#transhash').empty();
-        if ($("#accounts2 option:selected").text() == "") {
-            alert("Select one account !");
-        }
-        else {
-            $(".loader").show();
-            $('button').prop('disabled', true);
-            $.post("/gettrans", {
-                account: $("#accounts2 option:selected").text()
-            }, function(data) {
-                $(".loader").hide();
-                $('button').prop('disabled', false);
-                for (x in data) {
-                    $('#transhash').append($('<option>', {
-                        value: 1,
-                        text: data[x]
-                    }));
-                }
-            });
-        }
-
-    });
-
     $("#btn1").click(function() {
-        if ($("#transhash option:selected").text() == "") {
-            alert("There is no hash code !");
+        if ($("#accounts option:selected").text() == "") {
+            alert("There is no account selected !");
         }
         else {
             $(".loader").show();
             $('button').prop('disabled', true);
             $.post("/getresult", {
-                hash: $("#transhash option:selected").text()
+                account: $("#accounts option:selected").text()
             }, function(data) {
                 $(".loader").hide();
                 $('button').prop('disabled', false);
-                if (data.contractAddress != null) {
-                    $("#result").html("Deploy a contract at " + data.contractAddress);
-                }
-                else {
-                    $("#result").html("Contract Address : " + data.logs[0].address);
-                }
+                data.forEach((element)=>{
+                    switch(element.args.inf){
+                    case "confirm success":
+                        $("#result").append('於 '+new Date(element.args.timestamp*1000).toDateString() + '  合約確認' + '</br>');
+                        break;
+                    case "revoke the contract":
+                        $("#result").append('於 '+new Date(element.args.timestamp*1000).toDateString() + '  撤銷合約' + '</br>');
+                    }
+                })
             });
         }
     });
---
+++ public/trans.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+
+    $(".loader").hide();
+    $('.trans').prop('disabled', true);
+    $.post("/getaccount", function(data) {
+        data.forEach(function(element) {
+            $('.select-account').append($('<option>', {
+                value: 1,
+                text: element
+            }));
+        }, this);
+    });
+    $("#accounts").change(() => {
+        $('.trans').prop('disabled', true);
+    });
+    $("#compile").click(function() {
+        $(".loader").show();
+        $('button').prop('disabled', true);
+        $.post("/compile", function(data) {
+            $('button').prop('disabled', false);
+            $('.trans').prop('disabled', true);
+            $(".loader").hide();
+        });
+    });
+
+    $("#deploy").click(function() {
+        if ($("#accounts option:selected").text() == "") {
+            alert("Select one account !");
+        }
+        else {
+            $(".loader").show();
+            $('button').prop('disabled', true);
+            $.post("/deploy", {
+                account: $("#accounts option:selected").text()
+            }, function(data) {
+                $(".loader").hide();
+                $('button').prop('disabled', false);
+            });
+        }
+    });
+
+    $("#trans1").click(function() {
+        if ($("#accounts option:selected").text() == "") {
+            alert("Select one account !");
+        }
+        else {
+            $(".loader").show();
+            $('button').prop('disabled', true);
+            $.post("/trans", {
+                account: $("#accounts option:selected").text(),
+                time: 1
+            }, function(data) {
+                $(".loader").hide();
+                $('button').prop('disabled', false);
+            });
+        }
+    });
+
+    $("#trans2").click(function() {
+        if ($("#accounts option:selected").text() == "") {
+            alert("Select one account !");
+        }
+        else {
+            $(".loader").show();
+            $('button').prop('disabled', true);
+            $.post("/trans", {
+                account: $("#accounts option:selected").text(),
+                time: 2
+            }, function(data) {
+                $(".loader").hide();
+                $('button').prop('disabled', false);
+            });
+        }
+    });
+});
