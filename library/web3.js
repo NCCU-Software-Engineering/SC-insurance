5eaa76e885f953e08fca6894d896ef22a04d0600
autumn <dennis456852@gmail.com>
2017-07-30 20:32:40 +0800

    remote blockchain

--- library/web3.js
+++ library/web3.js
@@ -1,4 +1,4 @@
 var Web3 = require('web3')
-var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
-
+//var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+var web3 = new Web3(new Web3.providers.HttpProvider('http://140.119.163.66:8547'))
 module.exports = web3
