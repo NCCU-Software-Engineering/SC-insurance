15d133678cb457310bf508c0e589d576eb41c5d7
nidhogg5 <nidhogg5@yahoo.com.tw>
2017-07-31 14:50:38 +0800

    credentials

--- library/web3.js
+++ library/web3.js
@@ -1,4 +1,10 @@
+var credentials = require('./credentials.js');
 var Web3 = require('web3')
+
+//testrpc
 //var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
-var web3 = new Web3(new Web3.providers.HttpProvider('http://140.119.163.66:8547'))
+
+//testnet
+var web3 = new Web3(new Web3.providers.HttpProvider(credentials.web3.HttpProvider))
+
 module.exports = web3
