3f5783e659d4d03d225e56beebdb94d64e39eb3a
nidhogg5 <nidhogg5@yahoo.com.tw>
2017-08-30 18:13:50 +0800

    保費試算

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,10 @@ router.get('/', function (req, res, next) {
     res.render('index', { user_name: req.session.user_name })
 })
 
+router.get('/trial', function (req, res, next) {
+    res.render('trial', { user_name: req.session.user_name })
+})
+
 router.get('/buy', function (req, res, next) {
     if (req.session.user_ID && req.session.user_name) {
         res.render('buy', { user_name: req.session.user_name })
