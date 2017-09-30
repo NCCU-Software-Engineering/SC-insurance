b980a1ac9b2422ebfa4d664d273acd2293e8b5cb
nidhogg5 <nidhogg5@yahoo.com.tw>
2017-10-01 00:25:33 +0800

    bower

--- app.js
+++ app.js
@@ -1,65 +1,73 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
+var express = require('express')
+var session = require('express-session')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+
+var path = require('path')
+var favicon = require('serve-favicon')
+var bodyParser = require('body-parser')
+
 var credentials = require('./library/credentials.js')
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var index = require('./routes/index')
+var users = require('./routes/users')
 
-var app = express();
+var app = express()
 
 //view engine setup
 var handlebars = require('express-handlebars').create({
     defaultLayout: 'main',
     helpers: {
         section: function (name, options) {
-            if (!this._sections) this._sections = {};
-             this._sections[name] = options.fn(this);
-            return null;
+            if (!this._sections) this._sections = {}
+             this._sections[name] = options.fn(this)
+            return null
         }
     }
-});
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
+})
+app.engine('handlebars', handlebars.engine)
+app.set('view engine', 'handlebars')
 
 //uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'imgs', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
+app.use(favicon(path.join(__dirname, 'public', 'imgs', 'favicon.ico')))
+app.use(logger('dev'))
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: false
-}));
+}))
+
+//cookie session
 app.use(cookieParser(credentials.cookieSecret));
 app.use(session({
   secret: 'B54C9B842DD16',
   cookie: { maxAge: 60 * 100000 }
-}));
-app.use(express.static(path.join(__dirname, 'public')));
+}))
 
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'bower_components', 'tether', 'dist')))
+app.use(express.static(path.join(__dirname, 'bower_components', 'bootstrap', 'dist')))
+app.use(express.static(path.join(__dirname, 'bower_components', 'jquery', 'dist')))
+app.use(express.static(path.join(__dirname, 'bower_components', 'sweetalert2', 'dist')))
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', index)
+app.use('/users', users)
 
 //catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    res.status(404);
-    res.render('404');
-});
+    res.status(404)
+    res.render('404')
+})
 
 //error handler
 app.use(function (err, req, res, next) {
-    console.error(err.stack);
-    res.status(500);
-    res.render('500');
-});
+    console.error(err.stack)
+    res.status(500)
+    res.render('500')
+})
 
 app.listen(app.get('port'), function () {
     console.log('Express started on http://localhost:' + app.get('port') + '\n' +
-        'press Ctrl-C to terminate');
-});
+        'press Ctrl-C to terminate')
+})
 
-module.exports = app;
+module.exports = app
--- public/js/base.js
+++ public/js/base.js
@@ -1,14 +1,7 @@
-var customInterpolationApp = angular.module('customInterpolationApp', []);
-
-customInterpolationApp.config(function ($interpolateProvider) {
-    $interpolateProvider.startSymbol('[[');
-    $interpolateProvider.endSymbol(']]');
-});
-
 if ($('#user_name').text()) {
-    $('.sign').show();
-    $('.unsign').hide();
+    $('.sign').show()
+    $('.unsign').hide()
 } else {
-    $('.unsign').show();
-    $('.sign').hide();
+    $('.unsign').show()
+    $('.sign').hide()
 }
