c5b3de8ca02498c7602eb8109a7669f7b8c1f0c0
nidhogg5 <nidhogg5@yahoo.com.tw>
2018-01-21 22:53:23 +0800

    web3更新

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -1,11 +1,44 @@
 $(function () {
 
     //顯示教學
-    if(typeof mist == 'undefined') {
-        $('#guide').show();
+    if (typeof mist == 'undefined') {
+        $('#guide').show()
     }
     //顯示付款
     else {
-        $('#wallet').show();
+        $('#wallet').show()
+
+        web3 = new Web3(web3.currentProvider);
+
+        $('#platform').html(mist.platform)
+
+        mist.menu.add('tkrzU1', {
+            name: 'My Meny Entry',
+            badge: 50,
+            position: 2,
+            selected: true,
+        }, function () {
+            console.log('我被按下了耶')
+        })
+
+        $('#requestAccount').click(function () {
+            mist.menu.update()
+            mist.requestAccount(function (e, address) {
+                if (e)
+                    console.error(e)
+                else
+                    console.log('Added new account', address);
+            })
+        })
+
+        $('#bip').click(function () {
+            mist.sounds.bip()
+        })
+        $('#bloop').click(function () {
+            mist.sounds.bloop()
+        })
+        $('#invite').click(function () {
+            mist.sounds.invite()
+        })
     }
 })
